7c1a2fd8ca65e2559b6f88df28ea7dddd38b292c
Dienaba <dienacisse5678@gmail.com>
2026-07-18 01:11:34 +0000

    Ajout de STATIC_ROOT pour collectstatic

--- boutique/settings.py
+++ boutique/settings.py
@@ -117,11 +117,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+# --- Fichiers statiques (CSS, JS, images) ---
+STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'   # là où collectstatic va tout rassembler
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',                 # là où tu ranges tes fichiers source (vendor/adminlte/...)
+]
 
 # Fichiers médias (photos produits ajoutées via l'admin)
 MEDIA_URL = 'media/'
+
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
